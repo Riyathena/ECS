bde8e7aee259bf3d8adc4a199112052a6f9122dd
Priyanka <priyanka@scanrev.com>
2021-12-11 16:23:18 +0530

    changes

--- java.js
+++ java.js
@@ -4,3 +4,16 @@ var message="My name is " + name + " and I am " + age  +" years old! ";
 
 alert(message);
 console.log(message);
+var sum =10+5;
+console.log(sum);
+
+
+var val1=23;
+var val2 ="23";
+if(val1===val2)
+{
+    console.log("these are the same ");
+}
+else{
+    console.log("one of these is not like the other...");
+}
